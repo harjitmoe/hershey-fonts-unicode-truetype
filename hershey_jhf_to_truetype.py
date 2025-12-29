054a38476b1734593ba9e85dec9a144393b7a754
HarJIT <harjitmoe@outlook.com>
2025-12-29 21:27:08 +0000

    Fixed some of the fonts displaying with a ridiculous default line height.

--- hershey_jhf_to_truetype.py
+++ hershey_jhf_to_truetype.py
@@ -219,4 +219,5 @@ for fontname in fontnames:
         for ucs, glyph in sorted(glyphs.items()):
             print(glyph, file=fd)
         print("</font></defs></svg>", file=fd)
-    subprocess.call(["fontforge", "-quiet", "-lang=ff", "-c", "Open($1); SetFontNames('', '', $3, '', $4); SetTTFName(0x409, 2, $5); Generate($2)", f"obj/{fontname}.svg", f"dist/Hershey{fontname}.ttf", friendlyname, copying_notice, friendlyvariant])
+    # Note that `UseTypoMetrics` seemingly cannot be set from FontForge's native script language
+    subprocess.call(["fontforge", "-quiet", "-lang=py", "-c", "f = open(argv[1]); f.fullname = argv[3]; f.copyright = argv[4]; f.appendSFNTName(0x409, 2, argv[5]); f.os2_use_typo_metrics = True; f.generate(argv[2])", f"obj/{fontname}.svg", f"dist/Hershey{fontname}.ttf", friendlyname, copying_notice, friendlyvariant])
